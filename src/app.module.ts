e576367780e7dfbc7411e3a263ea5c25c0982263
Jayasree <jayasreee275@gmail.com>
2025-11-21 18:03:37 +0530

    changes in typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,17 +9,17 @@ import { UsersModule } from './modules/users/users.module';
 import { RolesModule } from './modules/roles/roles.module';
 import { PermissionsModule } from './modules/permissions/permission.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { typeormConfig } from './config/typeorm.config';
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'Jayasree275',
-      database: 'vmms',
-      autoLoadEntities: true,
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal:true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject:[ConfigService],
+      useFactory:typeormConfig,
     }),
     MachinesModule,
     SlotsModule,
--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,13 +1,13 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { ConfigService } from '@nestjs/config';
 
-export const typeormConfig = async (configService: ConfigService) => ({
-  type: 'postgres',
-  host: configService.get('DB_HOST'),
+export const typeormConfig = async (configService: ConfigService): Promise<TypeOrmModuleOptions> => ({
+  type: 'postgres' as const,
+  host: configService.get<string>('DB_HOST'),
   port: configService.get<number>('DB_PORT'),
-  username: configService.get('DB_USER'),
-  password: configService.get('DB_PASS'),
-  database: configService.get('DB_NAME'),
+  username: configService.get<string>('DB_USER'),
+  password: configService.get<string>('DB_PASS'),
+  database: configService.get<string>('DB_NAME'),
   autoLoadEntities: true,
   synchronize: true,
 });
